39356fef78ed36df791103bd6168355f27f80b42
John DoeYvesCharvis <charvisyves@ymail.com>
2017-03-24 17:14:54 +0100

    BASEOK

--- pages/admin/category/postssingle.php
+++ pages/admin/category/postssingle.php
@@ -1,28 +1,27 @@
 <?php
-	$app = App::getInstance();
+    $app = App::getInstance();
 
-	if ($_POST) {
-		if (!empty($_POST['id'] && $_POST['titre'])) {
-			$res = $app->getTable('category')->update(
-				$_POST['id'], 
-				["titre"=>$_POST['titre'], 
-				]);
-			if ($res) {				
-				header("location: admin.php?p=category.single");
-				
-			}
-		}
-	}
-	$category = $app->getTable('category')->find($_GET['id']);
-	if ($category===false) {
-		$app->notFound();
-	}
-	$app->titre = $category->titre;
+    if ($_POST) {
+        if (!empty($_POST['id'] && $_POST['titre'])) {
+            $res = $app->getTable('category')->update($_POST['id'], ['titre'=>$_POST['titre']]);
+            if ($res) {
+                //message flash
+                header('location: admin.php?p=category.edit');
+
+            }
+        }
+    }
+
+    $category = $app->getTable('category')->find($_GET['id']);
+    if ($category===false) {
+        $app->notFound();
+    }
+    $app->titre = $category->titre;
 ?>
 
-<form method="post" action="admin.php?p=posts.single&id=<?= $category->id; ?>">
-	<input type="hidden" name="id" value="<?= $category->id; ?>">
-	<input class="form-control" type="text" name="titre" value="<?= $category->titre; ?>">
-	<input class="btn btn-primary" type="submit" name="">
+<h2>Modifier</h2>
+<form method="post">
+    <input type="hidden" name="id" value="<?= $category->id; ?>">
+    <input type="text" class="form-control" name="titre" value="<?= $category->titre; ?>">
+    <input type="submit" class="btn btn-primary">
 </form>
-
