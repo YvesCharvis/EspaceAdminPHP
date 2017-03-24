be72eb757fa46ce57f3cab6d62edf4a24df0eb5a
John DoeYvesCharvis <charvisyves@ymail.com>
2017-03-24 09:36:22 +0100

    first commit

---
+++ web/index.php
@@ -0,0 +1,22 @@
+<?php
+define('ROOT', dirname(__DIR__));
+require ROOT.'/app/App.php';
+App::load();
+
+if (isset($_GET['p'])) {
+	$page = $_GET['p'];
+}else{
+	$page = "home";
+}
+
+
+ob_start();
+if ($page==='home') {
+	require ROOT.'/pages/posts/home.php';
+}if ($page==='posts.category') {
+	require ROOT.'/pages/posts/category.php';
+}if ($page==='posts.single') {
+	require ROOT.'/pages/posts/single.php';
+}
+$content = ob_get_clean();
+require ROOT.'/pages/templates/default.php'; 
