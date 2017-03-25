0ed7feb786e4d8762adbdddcd0fa3dc7091424b6
John DoeYvesCharvis <charvisyves@ymail.com>
2017-03-25 09:04:01 +0100

    reload_publicweb

--- pages/templates/default.php
+++ pages/templates/default.php
@@ -3,49 +3,41 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
     <title><?= App::getInstance()->titre; ?></title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-
+                <!-- Bootstrap & materialize core CSS -->
+      <link rel="stylesheet" type="text/css" href="frameworks/bootstrap.min.css">
+     <!--  <link rel="stylesheet" type="text/css" href="frameworks/materialize.min.css"> -->
+      <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
-
-  <body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
           <a class="navbar-brand" href="index.php">newapp</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-
-          <ul class="nav navbar-nav">
-
-            <li><a href="index.php?p=<?=$connect ?>"><?= $connect ?></a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-
-    <div class="container">
-
+    </div>
+  <div id="navbar" class="collapse navbar-collapse">
+<ul class="nav navbar-nav">
+    <li><a href="index.php?p=<?=$connect ?>"><?= $connect ?></a></li>
+</ul>
+  </div><!--/.nav-collapse -->
+  </div>
+</nav>
+  <div class="container">
       <div class="starter-template">
         <?= $content; ?>
       </div>
 
-    </div><!-- /.container -->
-
-  </body>
+  </div><!-- /.container -->
+</body>
+                <!-- SCRIPTS LOADS -->
+<!-- <script type="text/javascript" hrel="scripts/bootstrap.min.js"></script>
+     <script type="text/javascript" hrel="jquery-3.2.0.min.js"></script> -->
 </html>
